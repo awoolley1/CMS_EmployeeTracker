7301080ae8b45eabaf202f60ed29331a278744af
Local Al <alexander.s.woolley@gmail.com>
2021-02-03 19:41:44 -0700

    .prompt addeD

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-const inquirer = require("inquirer")
-const mysql = require("mysql")
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employeetracker_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  //runPrompt();
+});
+
+function runPrompt() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View department",
+        "View role",
+        "View employee",
+        "Update role for an existing employee",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add department":
+          addDepartment();
+          break;
+
+        case "Add role":
+          addRole();
+          break;
+
+        case "Add employee":
+          addEmployee();
+          break;
+
+        case "View department":
+          viewDepartment();
+          break;
+
+        case "View role":
+          viewRole();
+          break;
+
+        case "View employee":
+          viewEmployee();
+          break;
+
+        case "Update role for an existing employee":
+          updateEmployee();
+          break;
+      }
+    });
+}
