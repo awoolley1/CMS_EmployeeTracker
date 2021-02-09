e9e70519c2edf10096cbc0e544ad3c6dfc8f7c1c
Local Al <alexander.s.woolley@gmail.com>
2021-02-09 14:28:30 -0700

    file cleanup

--- dbSeeds.sql
+++ dbSeeds.sql
@@ -4,17 +4,6 @@ CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
 
-CREATE TABLE employee(
-id INTEGER(1) AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES department(id)
-);
-
 CREATE TABLE department(
 id INTEGER(1) AUTO_INCREMENT NOT NULL,
 name VARCHAR(30),
@@ -29,3 +18,19 @@ department_id INTEGER,
 PRIMARY KEY (id),
 FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee(
+id INTEGER(1) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES department(id)
+);
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
+
